ca0a51d331285e3b8290984f49259c93ff920247
DArturchik <58793380+DArturchik@users.noreply.github.com>
2020-02-02 17:33:25 +0200

    asp_net_mvc

---
+++ WebApplication2/WebApplication2/Controllers/AttrController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace WebApplication2.Controllers
+{
+    public class AttrController : Controller
+    {
+        public ActionResult FeedBack_Res() 
+        {
+            return View();
+        }
+        [HttpPost]
+        public ViewResult Questionnaire_Res(string FName, string LName, string Male, string Female,string Bachelor, string Master)
+        {
+
+            ViewBag._FName = FName;
+            ViewBag._LName = LName;
+
+            if (Male=="true") 
+            {
+                ViewBag._Male = Male;
+                if (Female == "true") 
+                {
+                    ViewBag._Female = "";
+
+                }
+                
+            }
+            if (Female == "true")
+            {
+                ViewBag._Female = Female;
+                if (Male == "true")
+                {
+                    ViewBag._Male = "";
+
+                }
+
+
+            }
+
+            if (Bachelor == "true")
+            {
+                ViewBag._Bachelor = Bachelor;
+                if (Master == "true")
+                {
+                    ViewBag._Master = "";
+                }
+            }
+            if (Master == "true")
+            {
+                ViewBag._Master = Master;
+                if (Bachelor == "true")
+                {
+                    ViewBag._Bachelor = "";
+                }
+            }
+
+
+
+
+
+
+            return View("~/Views/Attr/Questionnaire_Res.cshtml");
+        }
+        
+
+    }
+}
---
+++ WebApplication2/WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly string content = String.Format("Trade In: Trade‑in values vary. iPhone 11 and iPhone 11 Pro promotional pricing is after trade‑in of iPhone 8 Plus and iPhone X in good condition. Additional trade‑in values require purchase of a new iPhone, subject to availability and limits. Must be at least 18. Apple or its trade-in partners reserve the right to refuse or limit any Trade In transaction for any reason. In‑store trade‑in requires presentation of a valid, government-issued photo ID (local law may require saving this information). Sales tax may be assessed on full value of new iPhone. Additional terms from Apple or Apple’s trade-in partners may apply. Monthly pricing: Available to qualified customers and requires 0% APR, 24-month installment loan with Citizens One or Apple Card Monthly Installments and iPhone activation with AT&T, Sprint, T-Mobile, or Verizon. Taxes and shipping not included. Additional Apple Card Monthly Installments terms are in the Customer Agreement. Additional iPhone Payments terms are here.");
+
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Task #1";
+            ViewBag.ContentText = content;
+            return View();
+        }
+        public ActionResult ArticlesPage() 
+        {
+            return View();
+        }
+        public ActionResult Feedback() 
+        {
+            return View();
+        }
+        [HttpPost]
+        public ViewResult _Action_FeedBack(string Name,DateTime Date,string Msg) 
+        {
+
+            ViewBag._Name = Name;
+            ViewBag._Date = Date.ToString("yyyy-MM-dd");
+            ViewBag._Msg = Msg;
+            
+   
+            return View("~/Views/Attr/FeedBack_Res.cshtml");
+        }
+
+        public ActionResult Questionnaire() 
+        {
+            
+            return View();
+        }
+        
+
+
+    }
+}
